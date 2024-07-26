d5e479a9de6307944c16ad36b1bfd19cfeef34a7
BrainNotFoundException <lavjhn@gmail.com>
2024-07-26 11:55:13 +0530

    TwoSum

---
+++ TwoSum/TwoSum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l=[]
+        found=False
+        for i in range(len(nums)):
+          for j in range(i+1, len(nums)):
+
+            if nums[i]+nums[j]==target:
+              l=[i,j]
+              found=True
+              break
+          if found:
+            break
+        return l
