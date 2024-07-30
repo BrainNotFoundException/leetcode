33c0e69c23d98dbf5023b7afbc19f3c8e32e873e
BrainNotFoundException <lavjhn@gmail.com>
2024-07-30 09:58:10 +0530

    TwoSum - changed list in function

--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         l=[]
         found=False
         for i in range(len(nums)):
