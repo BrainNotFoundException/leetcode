a59f962b73478f06a9a4aa27b30614cb34399981
BrainNotFoundException <lavjhn@gmail.com>
2024-09-04 14:28:45 +0530

    TwoSum - HashMap

--- TwoSum/TwoSum.py
+++ TwoSum/TwoSum.py
@@ -1,3 +1,47 @@
+## HASH MAP APPROACH
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        len_n = len(nums)
+        nums_map = {}
+        
+        for i in range(len_n):
+
+            nums_map[nums[i]] = i
+        
+        for i in range(len_n):
+
+            j = target - nums[i]
+            
+            if j in nums_map and nums_map[j]!=i:
+
+                return [i, nums_map[j]]
+
+        return []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##INEFFICIENT
+
+'''
+
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         l=[]
@@ -13,3 +57,5 @@ class Solution:
             break
 
         return l
+
+'''
