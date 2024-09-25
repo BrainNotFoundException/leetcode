785d1bc0331f5e31e46765b8839027184b9f08df
BrainNotFoundException <lavjhn@gmail.com>
2024-09-25 09:15:37 +0530

    LetterCombinationsOfAPhoneNumber - fixed list

--- LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumber.py
+++ LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumber.py
@@ -15,7 +15,7 @@ class Solution:
                 self.getCombo(n[1:], s, num_map)
             s = s[:-1]
 
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits: str) -> list[str]:
             
         num_map = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
 
