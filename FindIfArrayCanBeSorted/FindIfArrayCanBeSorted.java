8264f39c752a8e70dd3a89743bdff35f39635c61
BrainNotFoundException <lavjhn@gmail.com>
2024-11-06 23:17:18 +0530

    FindIfArrayCanBeSorted

---
+++ FindIfArrayCanBeSorted/FindIfArrayCanBeSorted.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+
+        int cur_sma = nums[0];
+        int cur_lar = nums[0];
+        int prev_lar = 0;
+        int prev_bits = Integer.bitCount(nums[0]);
+
+        for (int i=1; i<nums.length; i++){
+
+            if (prev_bits == Integer.bitCount(nums[i])){
+                
+                if (cur_sma>nums[i]){
+                    cur_sma=nums[i];
+                }
+                if (cur_lar<nums[i]){
+                    cur_lar=nums[i];
+                }
+
+            }else{
+
+                if (cur_sma<prev_lar){
+                    return false;
+                }
+                prev_lar = cur_lar;
+                prev_bits = Integer.bitCount(nums[i]);
+                cur_lar = nums[i];
+                cur_sma = nums[i];
+            }
+
+        }
+
+        return cur_sma >= prev_lar;
+        
+    }
+}
