7053a8a8788a00db94de11304e2c246354010875
BrainNotFoundException <lavjhn@gmail.com>
2025-01-09 11:01:22 +0530

    Count Prefix And Suffix Pairs I

---
+++ Easy/CountPrefixAndSuffixPairsI/CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,21 @@
+class CountPrefixAndSuffixPairsI {
+    public int countPrefixSuffixPairs(String[] words) {
+        
+        int count = 0;
+
+        for(int i=0; i<words.length; i++){
+            for(int j=i+1; j<words.length; j++){
+                if (isPrefixAndSuffix(words[i], words[j])){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
+
+    boolean isPrefixAndSuffix(String str1, String str2){
+        return str2.startsWith(str1) && str2.endsWith(str1);
+    }
+}
