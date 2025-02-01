59813ecd84795aa9573fab0f401d7aa37a2756ba
BrainNotFoundException <lavjhn@gmail.com>
2025-02-01 21:00:42 +0530

    Special Array 1

---
+++ Easy/SpecialArrayI/SpecialArrayI.java
@@ -0,0 +1,10 @@
+class SpecialArrayI {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=1; i<nums.length; i++){
+            if(nums[i-1]%2==nums[i]%2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
