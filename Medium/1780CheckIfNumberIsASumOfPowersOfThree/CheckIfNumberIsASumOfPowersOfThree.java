0d3c56a7a20bc39118fddd0acd1ab9a5b2747aff
BrainNotFoundException <lavjhn@gmail.com>
2025-03-04 11:13:22 +0530

    1780 Check if Number is a Sum of Powers of Three

---
+++ Medium/1780CheckIfNumberIsASumOfPowersOfThree/CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,14 @@
+class CheckIfNumberIsASumOfPowersOfThree {
+    public boolean checkPowersOfThree(int n) {
+        
+        while (n>0){
+            if(n%3==2){
+                return false;
+            }
+            n=n/3;
+        }
+
+        return true;
+
+    }
+}
