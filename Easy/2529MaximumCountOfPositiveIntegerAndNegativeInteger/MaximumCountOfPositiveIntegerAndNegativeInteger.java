a5d745b286c90264faf48c840608cc526081a364
BrainNotFoundException <lavjhn@gmail.com>
2025-03-12 13:24:15 +0530

    2529 Maximum Count of Positive Integer and Negative Integer

---
+++ Easy/2529MaximumCountOfPositiveIntegerAndNegativeInteger/MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -0,0 +1,16 @@
+class MaximumCountOfPositiveIntegerAndNegativeInteger {
+    public int maximumCount(int[] nums) {
+        
+        int pos=0, neg=0;
+        for(int i : nums){
+            if(i>0){
+                pos++;
+            }else if(i<0){
+                neg++;
+            }
+        }
+
+        return pos>neg ? pos : neg;
+
+    }
+}
